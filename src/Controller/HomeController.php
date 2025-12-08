84e10977cb3454d307d36d9a0b03e9db65a6d33a
Maxence192003 <maxence192003@gmail.com>
2025-12-08 09:57:55 +0100

    Suppression onglet

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -20,4 +20,29 @@ class HomeController extends AbstractController
             'user' => $this->getUser(),
         ]);
     }
+
+    #[Route('/patients', name: 'app_patients')]
+    public function patients(): Response
+    {
+        return $this->render('home/page/patients.html.twig');
+    }
+
+    #[Route('/liste_attente', name: 'app_liste_att')]
+    public function liste_att(): Response
+    {
+        return $this->render('home/page/liste_attente.html.twig');
+    }
+    
+    #[Route('/greffons', name: 'app_greffons')]
+    public function greffons(): Response
+    {
+        return $this->render('home/page/greffons.html.twig');
+    }
+
+    #[Route('/analyse', name: 'app_analyse')]
+    public function analyse(): Response
+    {
+        return $this->render('home/page/analyse.html.twig');
+    }
+
 }
